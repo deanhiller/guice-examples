57fa71cb0ac67166e8950a6185beb6519b116206
Dean Hiller <dean.hiller@gmail.com>
2016-07-07 14:49:59 -0600

    fix this up

--- example/src/main/java/org/company/guicing/BasicGuiceMain.java
+++ example/src/main/java/org/company/guicing/BasicGuiceMain.java
@@ -11,14 +11,14 @@ import java.util.List;
  * STEP 1: Draw design on whiteboard from App.class on down via reading the constructors
  * STEP 2: explain guice initialization and BESTModuleEver
  *     - only ONE SINGLE call to getInstance
- * STEP 3: show high level test and how to do the overrides
- * STEP 4: show low level test and how you can test any piece you want know since the design is correct (and can do it at any time)
+ * STEP 3: show high level test and how to do the overrides (IMAGINE you have 50 of these tests!!!!)
+ * STEP 4: show low level test and how you can test any piece you want know since the design is correct (and can do it at any time) (50 tests)
  * STEP 5: show @Provides method that has too many 'new' keywords in it (SAVE for later refactoring)
- * STEP 6: show @Before in test method that constructs with too many new operators with it
+ * STEP 6: show @Before in test method that constructs with too many new operators with it (50 tests)
  * STEP 7: Now, refactor Router.java such that steps 4, 5, and 6 all break!!!!
  *    -- add new biz class to constructor AND new dto to method!!
  * STEP 8: Now that those are broken, delete them(we can revert checkout anyways)
- * STEP 9: Make SocketLib depend on App and show the guice exception and then remove(comment out new constructor)
+ * STEP 9: Make SomeUtil depend on App and show the guice exception and then remove(comment out new constructor)
  * STEP 10: Throw exception from SomeUtil constructor in DEVELOPMENT mode
  * STEP 11: Throw exception from SomeUtil constructor in PRODUCTION mode
  * STEP 12: Now, swap out ALL SomeUtil very easily without affecting production code!!! for performance testing purposes (thanks Brian Dupras for runnig into this use-case)
