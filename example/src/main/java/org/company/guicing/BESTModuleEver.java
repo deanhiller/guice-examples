c184bc94d03c430599d3baf6bc2926b4a740d36f
Dean Hiller <dean.hiller@gmail.com>
2016-07-07 14:59:54 -0600

    done creating the example and steps through the example

--- example/src/main/java/org/company/guicing/BESTModuleEver.java
+++ example/src/main/java/org/company/guicing/BESTModuleEver.java
@@ -2,10 +2,20 @@ package org.company.guicing;
 
 import com.google.inject.Binder;
 import com.google.inject.Module;
+import com.google.inject.Provides;
+import org.company.guicing.lib.SomeUtil;
+
+import javax.inject.Singleton;
 
 public class BESTModuleEver implements Module {
     @Override
     public void configure(Binder binder) {
 
     }
+
+//    @Provides
+//    @Singleton
+//    public SomeUtil provideIt() {
+//        return new SomeUtil();
+//    }
 }
